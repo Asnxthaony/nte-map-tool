e3056e6c1c0a00d197b4ad928e3680e103b47e05
Asnxthaony <asnxthaony@outlook.com>
2026-05-01 14:56:50 +0800

    feat: draw text on marker

--- map.py
+++ map.py
@@ -1,6 +1,8 @@
 import json
+from pathlib import Path
+from typing import Optional, Tuple
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 MAP_EDGE_SIZE = 687134
 MAP_TILE_SIZE = 512
@@ -12,24 +14,19 @@ MAP_CENTER_X = -40532.004
 MAP_CENTER_Y = 131446.33
 
 
-def parse_asset_path(asset_path: str) -> str:
+def parse_asset_path(asset_path: str) -> Optional[Path]:
     """
     将形如 /Game/UI/UI/minimap/bigworldmap/map_bigworld_10001.map_bigworld_10001 的路径转换为 bigworldmap/map_bigworld_10001.webp
     """
 
-    marker = "/minimap/"
-    idx = asset_path.find(marker)
-    if idx != -1:
-        relative = asset_path[idx + len(marker) :]
-    else:
-        relative = asset_path.split("/")[-1]
+    marker = "/Game/UI/UI/minimap/"
+    if marker not in asset_path:
+        return None
 
-    if "." in relative:
-        base = relative.rsplit(".", 1)[0]
-    else:
-        base = relative
+    rel_path = asset_path.split(marker, 1)[1]
+    rel_path = rel_path.split(".", 1)[0]
 
-    return base + ".webp"
+    return Path(rel_path).with_suffix(".webp")
 
 
 def load_and_stitch(data, tile_size=512, output_path="output.webp"):
@@ -69,10 +66,11 @@ def load_and_stitch(data, tile_size=512, output_path="output.webp"):
 
     bigmap_image.save(output_path)
     print(f"拼接完成，保存至 {output_path}")
+
     return bigmap_image
 
 
-def world_pos_to_map_pos(world_x, world_y):
+def world_pos_to_map_pos(world_x: float, world_y: float) -> Tuple[int, int]:
     map_x = (world_x - MAP_CENTER_X) / MAP_SCALE
     map_y = (world_y - MAP_CENTER_Y) / MAP_SCALE
 
@@ -83,17 +81,22 @@ def world_pos_to_map_pos(world_x, world_y):
 
 
 def draw_marker(
-    image,
-    x,
-    y,
-    color="red",
-    radius=20,
+    image: Image.Image,
+    x: int,
+    y: int,
+    text: str = "",
+    color: str = "#fb3838",
+    text_color: str = "#0099ff",
+    radius: int = 20,
 ):
     draw = ImageDraw.Draw(image)
 
     draw.ellipse(
-        (x - radius, y - radius, x + radius, y + radius), fill=color, outline=color
+        (x - radius, y - radius, x + radius, y + radius), outline=color, width=2
     )
+    if text:
+        font = ImageFont.truetype("fonts/SimHei.ttf", 24)
+        draw.text((x + radius + 2, y - radius), text, fill=text_color, font=font)
 
 
 if __name__ == "__main__":
@@ -105,18 +108,18 @@ if __name__ == "__main__":
 
     # 载具兄弟-阶段1
     map_x, map_y = world_pos_to_map_pos(-101892.14, 93486.65)
-    draw_marker(bigmap, x=map_x, y=map_y, color="red", radius=20)
+    draw_marker(bigmap, x=map_x, y=map_y, text="载具兄弟-阶段1")
 
     # 载具兄弟-阶段2
     map_x, map_y = world_pos_to_map_pos(-101411.055, 75400.87)
-    draw_marker(bigmap, x=map_x, y=map_y, color="green", radius=20)
+    draw_marker(bigmap, x=map_x, y=map_y, text="载具兄弟-阶段2")
 
     # 载具兄弟-阶段3
     map_x, map_y = world_pos_to_map_pos(-127812.64, 68254.4)
-    draw_marker(bigmap, x=map_x, y=map_y, color="blue", radius=20)
+    draw_marker(bigmap, x=map_x, y=map_y, text="载具兄弟-阶段3")
 
     # 载具兄弟-阶段4/5
     map_x, map_y = world_pos_to_map_pos(-98927.58, 69289.6)
-    draw_marker(bigmap, x=map_x, y=map_y, color="orange", radius=20)
+    draw_marker(bigmap, x=map_x, y=map_y, text="载具兄弟-阶段4/5")
 
     bigmap.save("bigmap_with_marker.png")
